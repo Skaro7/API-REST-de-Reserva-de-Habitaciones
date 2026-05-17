58ef9972f2b88298e0ac161be959f85846fc3c72
Skaro7 <marc_tau72@hotmail.com>
2026-05-17 18:18:10 +0200

    feat: Add User entity with validation and JPA annotations

---
+++ src/main/java/com/reservadehabitaciones/hotel/entity/User.java
@@ -0,0 +1,29 @@
+package com.reservadehabitaciones.hotel.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    @Email
+    @NotBlank
+    private String email;
+
+}
