7ec8757256027744c6a6e39bd55046aeaa8c8256
Skaro7 <marc_tau72@hotmail.com>
2026-05-19 12:08:58 +0200

    feat: Add UserRepository for user entity data access

---
+++ src/main/java/com/reservadehabitaciones/hotel/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.reservadehabitaciones.hotel.repository;
+
+import com.reservadehabitaciones.hotel.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
+}
